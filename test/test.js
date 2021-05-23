1672078968c13b7b5fda9790d37f995396186e30
shaipa999 <81581929+shaipa999@users.noreply.github.com>
2021-05-23 10:10:29 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -5,13 +5,13 @@ const app = express();
 
 describe("GET http://20.81.102.53:8080/healthcheck", () => {
   it("respond with ok", (done) => {
-    request(app).get("20.81.102.53:8080/healthcheck").expect("ok", done);
+    request(app).get("https://www.google.com/").expect("ok", done);
   })
 });
 
 describe("GET http://20.81.102.53:8080/fetch", () => {
   it("respond with char - Rick", (done) => {
-    request(app).get("20.81.102.53:8080/fetch").expect("Rick Sanchez", done);
+    request(app).get("https://www.google.com/").expect("Rick Sanchez", done);
   })
 });
 
