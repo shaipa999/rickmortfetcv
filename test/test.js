d6f05d4c89dfae3bd1be72edd5255ef90365af06
shaipa999 <81581929+shaipa999@users.noreply.github.com>
2021-05-23 09:50:50 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,16 +1,17 @@
-const request = require("supertest");
-const express = require("express");
+const request = require('supertest');
+const express = require('express');
+
 const app = express();
 
 describe("GET http://20.81.102.53:8080/healthcheck", () => {
   it("respond with ok", (done) => {
-    request.get("http://20.81.102.53:8080/healthcheck").expect("ok", done);
+    request(app).get("http://20.81.102.53:8080/healthcheck").expect("ok", done);
   })
 });
 
 describe("GET http://20.81.102.53:8080/fetch", () => {
   it("respond with char - Rick", (done) => {
-    request.get("http://20.81.102.53:8080/fetch").expect("Rick Sanchez", done);
+    request(app).get("http://20.81.102.53:8080/fetch").expect("Rick Sanchez", done);
   })
 });
 
